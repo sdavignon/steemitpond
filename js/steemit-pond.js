c39fe0fe69e45fb1837180afba2fab448e75f612
Brian Howell <bigbhowell@gmail.com>
2016-07-28 13:40:07 -0500

    Webkit scrolling issue

--- js/steemit-pond.js
+++ js/steemit-pond.js
@@ -249,12 +249,6 @@ var SteemitPond = (function() {
 
 $( document ).ready(function() {
     if (window.WebSocket) {
-
-        var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-        if (iOS) {
-            $('#steemit-pond')..on("touchmove", false);
-        }
-
         SteemitPond.init();
     } else {
         alert('Websocket is not supported by your browser.');
