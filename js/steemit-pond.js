e6861f0f4b057e06b37460e29b235ed7aab6b7f7
sdavignon <sdavignon1@gmail.com>
2018-02-28 11:09:39 -0500

    Update steemit-pond.js

--- js/steemit-pond.js
+++ js/steemit-pond.js
@@ -6,7 +6,7 @@
 var SteemitPond = (function() {
 
     // Websocket
-    var server = 'wss://node.steem.ws';
+    var server = 'wss://steemd.steemit.com';
     var ws = new WebSocketWrapper(server);
     var steem = new SteemWrapper(ws);
 
